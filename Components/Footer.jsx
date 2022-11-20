74ddd09baa1f444a126999225d66b9184fc1be3a
Ahmed Bolarinwa <bolarinwaahmed22@gmail.com>
2022-11-20 19:28:28 +0100

    Styled footer and created the AuthContext

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import styles from './../styles/Footer.module.css';
 const Footer = () => {
     return ( 
-        <footer>
-            <div>
+        <footer className={styles.footer}>
+            <div className={styles.name}>
                 <h4>Anonify</h4>
             </div>
-            <div>
+            <div className={styles.text}>
                 <p>Copyright - Bolarinwa Ahmed 2022.</p>
             </div>
         </footer>
---
+++ Context/AuthContext.js
@@ -0,0 +1,41 @@
+import React, {createContext, useContext, useState, useEffect} from 'react';
+import {auth} from './../Utilities/firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+   return  useAuth(AuthContext);
+}
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsuscribe = onAuthStateChanged((auth, user) => {
+            setUser(user);
+            console.log(user);
+        });
+
+        return () => unsuscribe;
+    }, [])
+
+    const signup = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const values = {
+        user,
+        signup,
+        login,
+    }
+
+    return <AuthContext.Provider value={values}>
+        {children}
+    </AuthContext.Provider>
+
+}
